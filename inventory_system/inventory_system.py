6ae822ba027b54ad45e8cc005684053833ac3675
NIDHIRAM-SORDAR <alsaeedctg@gmail.com>
2025-04-09 07:05:55 +0000

    add enviromental var for upload loc

--- inventory_system/inventory_system.py
+++ inventory_system/inventory_system.py
@@ -5,6 +5,11 @@ import reflex as rx
 
 from . import styles
 from .pages import *
+import os
+
+
+# Set the environment variable
+os.environ["REFLEX_UPLOADED_FILES_DIR"] = "/assets/uploads"
 
 # Create the app.
 app = rx.App(
