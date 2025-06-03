d25bfdd3fc4469f76847bde1b9b1ec5ee0d147a0
NIDHIRAM-SORDAR <alsaeedctg@gmail.com>
2025-06-03 08:30:09 +0600

    need to work on logging

--- inventory_system/inventory_system.py
+++ inventory_system/inventory_system.py
@@ -8,16 +8,13 @@ import reflex as rx
 from inventory_system.logging.logging import setup_loguru
 
 from . import styles
-from .logging.audit_middleware import create_audit_logging_transformer
 from .pages import *
 
 # Set the environment variable
 os.environ["REFLEX_UPLOADED_FILES_DIR"] = "assets/uploads"
 setup_loguru()
 # Create the app.
-audit_transformer = create_audit_logging_transformer()
 app = rx.App(
     style=styles.base_style,
     stylesheets=styles.base_stylesheets,
-    api_transformer=audit_transformer,
 )
